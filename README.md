3e9fd6203347cd3096f8010845e29000f7136693
soumodiptab <sbose2019@gmail.com>
2021-11-25 01:41:59 +0530

    Some readjustments

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# multi-threading-problems
+# Multi-Threading-Problems
 A collection of problems solved using multi-threading
+
+[Google Doc](https://docs.google.com/document/d/1LH7CpG3SVDEIzdhZy4txEEdMF-oicjZPpAZt9xP3dLE/edit)
+## Q2: The Queue at the Polling Booth
+### Entity Diagram
+![Entity Diagram](/snapshots/evm_entity_diagram.png)
+### System Diagram
+![Thread Diagram](/snapshots/evm_thread_diagram.png)
--- queue-booth-problem.cpp
+++ queue-booth-problem.cpp
@@ -2,8 +2,8 @@
  * @file queue-booth-problem.cpp
  * @author Soumodipta Bose
  * @brief Multi Threading problem solving for the EVM problem
- * @version 0.1
- * @date 2021-11-18
+ * @version 1.0
+ * @date 2021-11-25
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -32,9 +32,7 @@ void print(string s)
 }
 void print_ln(string s)
 {
-    pthread_mutex_lock(&print_mutex);
     cout << s << endl;
-    pthread_mutex_unlock(&print_mutex);
 }
 int voter_id_generator()
 {
@@ -59,39 +57,51 @@ string booth_id(int id)
 }
 void highlight_red_ln(string message)
 {
+    pthread_mutex_lock(&print_mutex);
     cout << "\033[31m";
     print_ln(message);
     cout << "\033[0m";
+    pthread_mutex_unlock(&print_mutex);
 }
 void highlight_green_ln(string message)
 {
+    pthread_mutex_lock(&print_mutex);
     cout << "\033[32m";
     print_ln(message);
     cout << "\033[0m";
+    pthread_mutex_unlock(&print_mutex);
 }
 void highlight_blue_ln(string message)
 {
+    pthread_mutex_lock(&print_mutex);
     cout << "\033[34m";
     print_ln(message);
     cout << "\033[0m";
+    pthread_mutex_unlock(&print_mutex);
 }
 void highlight_cyan_ln(string message)
 {
+    pthread_mutex_lock(&print_mutex);
     cout << "\033[36m";
     print_ln(message);
     cout << "\033[0m";
+    pthread_mutex_unlock(&print_mutex);
 }
 void highlight_yellow_ln(string message)
 {
+    pthread_mutex_lock(&print_mutex);
     cout << "\033[33m";
     print_ln(message);
     cout << "\033[0m";
+    pthread_mutex_unlock(&print_mutex);
 }
 void highlight_purple_ln(string message)
 {
+    pthread_mutex_lock(&print_mutex);
     cout << "\033[35m";
     print_ln(message);
     cout << "\033[0m";
+    pthread_mutex_unlock(&print_mutex);
 }
 /***************************************************************************************************************************/
 /*                                                 ENTITY STRUCTURES & INITIALIZERS                                        */
@@ -105,15 +115,7 @@ struct Evm
 {
     pthread_t thread_id;
     int id;
-    /**
-     * @brief Total slots that are used to parallely vote
-     * 
-     */
     int slots;
-    /**
-     * @brief True - allocated False - not allocated
-     * 
-     */
     int status;
     int tokens;
     int vote_tokens;
@@ -151,10 +153,6 @@ struct Booth
     Evm *selected_evm;
     vector<Evm *> evms;
     vector<Voter *> voters;
-    /**
-     * @brief Protect the CS: casted_votes
-     * 
-     */
     pthread_mutex_t registration_mutex;
     pthread_mutex_t votes_mutex;
     pthread_mutex_t evm_mutex;
@@ -241,16 +239,16 @@ Booth *booth_init(int id, int voters, int number_of_evm, int slots)
 void polling_ready_evm(Booth *booth, int count, Evm *evm)
 {
     int evmid = evm->id;
-    highlight_green_ln(evm_id(evmid) + " called polling_ready_evm() for slot allocation");
+    highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " called polling_ready_evm() for slot allocation");
     pthread_mutex_lock(&(booth->evm_mutex));
     while (true)
     {
-        highlight_green_ln(evm_id(evmid) + " will try to fill slots now");
+        highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " will try to fill slots now");
         pthread_mutex_lock(&(booth->registration_mutex));
-        highlight_green_ln(evm_id(evmid) + " Checking if any voters arrived [ Registered voters: " + to_string(booth->registrations) + " ] [ Voters not registered : " + to_string(booth->unregistered_voters) + " ]");
+        highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " Checking if any voters arrived [ Registered voters: " + to_string(booth->registrations) + " ] [ Voters not registered : " + to_string(booth->unregistered_voters) + " ]");
         if (booth->registrations == 0 && booth->unregistered_voters == 0)
         {
-            highlight_green_ln(evm_id(evmid) + " All voters already assigned slots. Returning...");
+            highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " All voters already assigned slots. Returning...");
             pthread_mutex_unlock(&(booth->registration_mutex));
             pthread_mutex_unlock(&(booth->evm_mutex));
             break;
@@ -266,11 +264,11 @@ void polling_ready_evm(Booth *booth, int count, Evm *evm)
             evm->status = 1;
             booth->registrations -= count;
             booth->selected_evm = evm;
-            highlight_green_ln(evm_id(evmid) + " Sufficient voters arrived... waking all voters and releasing tokens: " + to_string(evm->tokens));
+            highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " Sufficient voters arrived... waking all voters and releasing tokens: " + to_string(evm->tokens));
             pthread_mutex_lock(&(evm->evm_mutex));
             pthread_cond_broadcast(&(booth->voter_to_evm));                         // tell voter_wait_for_evm to add voters to slots
             pthread_cond_wait(&(evm->evm_condition), &(booth->registration_mutex)); //wait for voter threads to reach slot
-            highlight_green_ln(evm_id(evmid) + " All slots have been allocated. Returning...");
+            highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " All slots have been allocated. Returning...");
             booth->selected_evm = NULL;
             pthread_mutex_unlock(&(booth->registration_mutex));
             pthread_mutex_unlock(&(booth->evm_mutex));
@@ -279,10 +277,10 @@ void polling_ready_evm(Booth *booth, int count, Evm *evm)
         }
         else
         {
-            highlight_green_ln(evm_id(evmid) + " Sleeping and waiting for sufficient voters to arrive");
+            highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " Sleeping and waiting for sufficient voters to arrive");
             pthread_cond_wait(&(booth->evm_to_voter), &(booth->registration_mutex));
             pthread_mutex_unlock(&(booth->registration_mutex));
-            highlight_green_ln(evm_id(evmid) + " Woken from sleep to check voters at booth");
+            highlight_green_ln(booth_id(booth->id) + evm_id(evmid) + " Woken from sleep to check voters at booth");
         }
     }
 }
@@ -295,7 +293,7 @@ void voter_wait_for_evm(Booth *booth, Voter *voter)
 {
     int voterid = voter->id;
     pthread_mutex_lock(&(booth->registration_mutex));
-    highlight_yellow_ln(voter_id(voterid) + " arrived at voter_wait_for_evm() for slot allocation");
+    highlight_yellow_ln(booth_id(booth->id) + voter_id(voterid) + " arrived at voter_wait_for_evm() for slot allocation");
     booth->unregistered_voters--;
     booth->registrations++;
     while (true)
@@ -306,25 +304,25 @@ void voter_wait_for_evm(Booth *booth, Voter *voter)
             {
                 booth->selected_evm->tokens -= 1;
                 voter->evm_ref = booth->selected_evm;
-                highlight_yellow_ln(voter_id(voterid) + " Token Acquired at " + evm_id(booth->selected_evm->id) + " moving towards evm...");
+                highlight_yellow_ln(booth_id(booth->id) + voter_id(voterid) + " Token Acquired at " + evm_id(booth->selected_evm->id) + " moving towards evm...");
+                highlight_green_ln(booth_id(booth->id) + evm_id(booth->selected_evm->id) + " Tokens left: " + to_string(booth->selected_evm->tokens));
                 if (booth->selected_evm->tokens == 0)
                     pthread_cond_signal(&(booth->selected_evm->evm_condition));
-                highlight_purple_ln("Tokens left: " + to_string(booth->selected_evm->tokens) + " at " + evm_id(booth->selected_evm->id));
                 pthread_mutex_unlock(&(booth->registration_mutex));
                 break;
             }
             else
             {
-                highlight_yellow_ln(voter_id(voterid) + " could not acquire token");
+                highlight_yellow_ln(booth_id(booth->id) + voter_id(voterid) + " could not acquire token");
             }
         }
         // wake a single evm and ask it to see if sufficient no. of voters have arrived
-        highlight_yellow_ln(voter_id(voterid) + " waking an evm to check voter arrival");
+        highlight_yellow_ln(booth_id(booth->id) + voter_id(voterid) + " waking an evm to check voter arrival");
         pthread_cond_signal(&booth->evm_to_voter);
         //wait for evm to say that it is accepting slots
-        highlight_yellow_ln(voter_id(voterid) + " Sleeping and waiting for evm to call");
+        highlight_yellow_ln(booth_id(booth->id) + voter_id(voterid) + " Sleeping and waiting for evm to call");
         pthread_cond_wait(&(booth->voter_to_evm), &(booth->registration_mutex));
-        highlight_yellow_ln(voter_id(voterid) + " Woken up by Evm and trying to acquire token");
+        highlight_yellow_ln(booth_id(booth->id) + voter_id(voterid) + " Woken up by Evm and trying to acquire token");
         pthread_mutex_unlock(&(booth->registration_mutex));
         pthread_mutex_lock(&(booth->registration_mutex));
     }
@@ -338,19 +336,19 @@ void voter_wait_for_evm(Booth *booth, Voter *voter)
 void voter_in_slot(Booth *booth, Voter *voter) // this is where voting will happen
 {
     Evm *evm = voter->evm_ref;
-    highlight_yellow_ln(voter_id(voter->id) + " has reached " + evm_id(evm->id) + " slot");
+    highlight_yellow_ln(booth_id(booth->id) + voter_id(voter->id) + " has reached " + evm_id(evm->id) + " slot");
     pthread_mutex_lock(&(evm->evm_mutex));
     evm->vote_tokens++;
     pthread_cond_signal(&evm->evm_condition);
     pthread_cond_wait(&(evm->voter_condition), &(evm->evm_mutex));
-    highlight_yellow_ln(voter_id(voter->id) + " Awoken by " + evm_id(evm->id) + " to cast vote");
+    highlight_yellow_ln(booth_id(booth->id) + voter_id(voter->id) + " Awoken by " + evm_id(evm->id) + " to cast vote");
     evm->vote_tokens--;
     pthread_mutex_lock(&(booth->votes_mutex));
     booth->casted_votes++;
     voter->vote_status = true;
     pthread_mutex_unlock(&(booth->votes_mutex));
     pthread_cond_signal(&evm->evm_condition);
-    highlight_yellow_ln(voter_id(voter->id) + " has casted its vote at " + evm_id(evm->id));
+    highlight_yellow_ln(booth_id(booth->id) + voter_id(voter->id) + " has casted its vote at " + evm_id(evm->id));
     pthread_mutex_unlock(&(evm->evm_mutex));
 }
 /***************************************************************************************************************************/
@@ -367,10 +365,10 @@ void *voter_thread(void *arg)
 {
     Voter *voter = (Voter *)arg;
     Booth *booth = voter->back_ref;
-    highlight_yellow_ln(voter_id(voter->id) + " thread created");
+    highlight_yellow_ln(booth_id(booth->id) + voter_id(voter->id) + " Voter arrived outside booth");
     voter_wait_for_evm(booth, voter);
     voter_in_slot(booth, voter);
-    highlight_yellow_ln(voter_id(voter->id) + " thread destroyed");
+    highlight_yellow_ln(booth_id(booth->id) + voter_id(voter->id) + " Voter has left booth");
 }
 /**
  * @brief Evm thread to call the various functions parallely
@@ -382,19 +380,19 @@ void *evm_thread(void *arg)
 {
     Evm *evm = (Evm *)arg;
     Booth *booth = evm->back_ref;
-    highlight_green_ln(evm_id(evm->id) + " is starting up...");
+    highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " is starting up...");
     while (true)
     {
         evm->status = 0;
         evm->slots = (rand() % min(range, booth->max_slots)) + 1; //number of slots for current cycle
         evm->tokens = evm->slots;
         evm->vote_tokens = 0;
-        highlight_green_ln(evm_id(evm->id) + " Slots free in this cycle : [ " + to_string(evm->slots) + " ]");
+        highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " Slots free in this cycle : [ " + to_string(evm->slots) + " ]");
         polling_ready_evm(booth, evm->slots, evm);
         pthread_mutex_lock(&(booth->votes_mutex));
         if (booth->total_voters == booth->casted_votes || !evm->status)
         {
-            highlight_green_ln(evm_id(evm->id) + " is shutting down...");
+            highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " is shutting down...");
             pthread_mutex_unlock(&(booth->votes_mutex));
             break;
         }
@@ -406,20 +404,20 @@ void *evm_thread(void *arg)
             if (evm->vote_tokens == evm->slots)
             {
                 pthread_cond_broadcast(&(evm->voter_condition));
-                highlight_green_ln(evm_id(evm->id) + " Waking all voters to cast their votes");
+                highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " Waking all voters to cast their votes");
                 pthread_mutex_unlock(&(evm->evm_mutex));
                 break;
             }
-            highlight_green_ln(evm_id(evm->id) + " Sleeping and waiting for all voters to arrive at slots");
+            highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " Sleeping and waiting for all voters to arrive at slots");
             pthread_cond_wait(&(evm->evm_condition), &(evm->evm_mutex));
-            highlight_green_ln(evm_id(evm->id) + " woken from sleep to check if all voters arrived at slots");
+            highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " woken from sleep to check if all voters arrived at slots");
             pthread_mutex_unlock(&(evm->evm_mutex));
         }
 
         while (true)
         {
             pthread_mutex_lock(&(evm->evm_mutex));
-            highlight_green_ln(evm_id(evm->id) + " votes casted: " + to_string(evm->vote_tokens));
+            highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " votes casted: " + to_string(evm->vote_tokens));
             if (evm->vote_tokens == 0)
             {
                 pthread_cond_broadcast(&(evm->voter_condition));
@@ -429,7 +427,7 @@ void *evm_thread(void *arg)
             pthread_cond_wait(&(evm->evm_condition), &(evm->evm_mutex));
             pthread_mutex_unlock(&(evm->evm_mutex));
         }
-        highlight_green_ln(evm_id(evm->id) + " has completed voting cycle");
+        highlight_green_ln(booth_id(booth->id) + evm_id(evm->id) + " has completed voting cycle");
     }
 }
 
@@ -442,7 +440,7 @@ void *evm_thread(void *arg)
 void *booth_thread(void *arg)
 {
     Booth *booth = (Booth *)arg;
-    highlight_cyan_ln("Created new booth thread :" + booth_id(booth->id));
+    highlight_cyan_ln(booth_id(booth->id) + " Booth is starting up...");
     for (int i = 0; i < booth->evms_number; i++)
     {
         pthread_create(&(booth->evms[i]->thread_id), NULL, evm_thread, booth->evms[i]);
@@ -474,7 +472,7 @@ void driver()
     vector<int> voters(booths);
     for (int i = 0; i < booths; i++)
     {
-        print_ln("Booth:" + to_string(i + 1));
+        print_ln("Booth:" + to_string(i));
         print_ln("Enter number of EVMS ?");
         cin >> evms[i];
         print_ln("Enter maximum number of slots per EVM ?");
