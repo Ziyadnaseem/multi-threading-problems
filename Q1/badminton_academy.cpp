c8663c08187f22e70cd7e107124e37cd4395e100
Kushal7788 <kushshah777888@gmail.com>
2021-11-27 20:35:13 +0530

    Added comments to Q1

--- Q1/badminton_academy.cpp
+++ Q1/badminton_academy.cpp
@@ -11,9 +11,9 @@
 using namespace std;
 
 int players_ready_count = 0;
-int refrees_ready_count = 0;
+int referees_ready_count = 0;
 int warm_up_players = 0;
-int refree_start = 0;
+int referee_start = 0;
 pthread_mutex_t organizer;
 pthread_mutex_t court;
 pthread_mutex_t print;
@@ -21,12 +21,13 @@ pthread_mutex_t warm;
 pthread_mutex_t start_game_m;
 pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 pthread_cond_t player_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t refree_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t referee_cond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t start_game_condition = PTHREAD_COND_INITIALIZER;
 int count_critical = 0;
 
 void display(string msg)
 {
+    // Function to print messages in terminal
     pthread_mutex_lock(&print);
     cout << msg << endl;
     pthread_mutex_unlock(&print);
@@ -40,6 +41,8 @@ void enterAcademy(string id)
 
 void playerMeetOrganizer(string id)
 {
+    // If more than 2 players meet oragnizer then the respective threads sleep
+    // until game starts for previous group
     pthread_mutex_lock(&organizer);
     if (players_ready_count >= 2)
     {
@@ -56,14 +59,16 @@ void playerMeetOrganizer(string id)
 
 void refMeetOrganizer(string id)
 {
+    // If more than 1 referee meet oragnizer then the respective threads sleep
+    // until game starts for previous group
     pthread_mutex_lock(&organizer);
-    if (refrees_ready_count >= 1)
+    if (referees_ready_count >= 1)
     {
-        pthread_cond_wait(&refree_cond, &organizer);
+        pthread_cond_wait(&referee_cond, &organizer);
     }
     else
     {
-        refrees_ready_count++;
+        referees_ready_count++;
     }
     string val = id + " Meeting Organizer";
     display(val);
@@ -72,6 +77,8 @@ void refMeetOrganizer(string id)
 
 void enterCourt(string id)
 {
+    // All threads entering will sleep until a total of 3 threads call this function
+    // The 3rd thread wakes up other 2 threads
     pthread_mutex_lock(&court);
     if (count_critical == 2)
     {
@@ -91,6 +98,8 @@ void enterCourt(string id)
 // Player Functions
 void warmUp(string id)
 {
+    // After warming up, thread increases the global counter and when warm_up_players == 2
+    // then wake up referee thread in startGame if it is in sleep
     string val = id + " Player Warming Up";
     display(val);
     sleep(1);
@@ -113,6 +122,10 @@ void adjustEquipment(string id)
 
 void startGame(string id)
 {
+    // Starts game for the repective group when both players are ready else sleeps until players get ready
+    // After starting game, decreases global pointers for ready players and referees
+    // and wakes up player and referee threads sleeping at respective meetOrganizer functions
+    // Allows organizer to meet with new groups again after startGame
     pthread_mutex_lock(&start_game_m);
     if (warm_up_players < 2)
     {
@@ -123,10 +136,10 @@ void startGame(string id)
     string val = id + " Refree Starting Game";
     display(val);
     players_ready_count -= 2;
-    refrees_ready_count--;
+    referees_ready_count--;
     pthread_cond_signal(&player_cond);
     pthread_cond_signal(&player_cond);
-    pthread_cond_signal(&refree_cond);
+    pthread_cond_signal(&referee_cond);
 
     pthread_mutex_unlock(&organizer);
     pthread_mutex_unlock(&start_game_m);
@@ -134,6 +147,7 @@ void startGame(string id)
 
 void *player(void *args)
 {
+    // Player thread
     int n_id = *(int *)args;
     string p_id = "P" + to_string(n_id);
     enterAcademy(p_id);
@@ -145,6 +159,7 @@ void *player(void *args)
 
 void *refree(void *args)
 {
+    // Refree thread
     int n_id = *(int *)args;
     string p_id = "R" + to_string(n_id);
     enterAcademy(p_id);
@@ -168,9 +183,11 @@ int main()
     int curr_player_count = 0;
     int curr_refree_count = 0;
     int last_ref = 0;
+    // loop until curr_people_count does not form n groups from input
     while (curr_people_count < 3 * n)
     {
         curr_people_count++;
+        // If more or equal referee count than 2 * total players count
         if (curr_refree_count >= (int)floor(curr_player_count / 2))
         {
             // Player thread here
@@ -187,9 +204,12 @@ int main()
             *curr_val = curr_refree_count;
             pthread_create(&pid[curr_people_count], NULL, refree, curr_val);
         }
+        // New person comes with random(0,999) % 3 second delay
+        // till then main thread sleeps
         int rand_val = rand() % 1000;
         sleep(rand_val % 3);
     }
+    // Main thread waiting for last group to finish the match
     pthread_join(pid[curr_people_count], NULL);
 
     return 0;
